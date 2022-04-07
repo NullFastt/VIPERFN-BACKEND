397a965aca27a164e1cd03f41fe2bde0ca8f5754
HugoFastt <>
2022-04-07 08:48:28 +0000

    Initial commit

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    host: 'localhost',
+    port: 5595,
+    directory: __dirname,
+    dir: __dirname,
+    season_num: 19,
+    banner: "https://media.discordapp.net/attachments/899550196420186112/960808136413949963/New_Project.jpg?width=705&height=397",
+
+    background: 'winter2021'
+    /*
+    season19 = chapter 3
+    winter2021 = Winterfest 2021
+    */
+}
